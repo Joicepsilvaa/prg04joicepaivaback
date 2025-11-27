55f44da73fdfe3509f3fe7b342192da24e669f5f
joicepaivasilvapereira@gmail.com <joicepaivasilvapereira@gmail.com>
2025-11-27 16:20:42 -0300

    Teste de commit

---
+++ src/main/java/br/com/ifba/Prg04backApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prg04backApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prg04backApplication.class, args);
+	}
+
+}
